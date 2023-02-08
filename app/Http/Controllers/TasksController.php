060e374a053b0bde95f5063e934f6fece51652aa
maki.maki <1b121128.ccs@gmail.com>
2023-02-08 02:08:20 +0000

    add column

---
+++ app/Http/Controllers/TasksController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Task;
+
+class TasksController extends Controller
+{
+    // getでtasks/にアクセスされた場合の「一覧表示処理」
+    public function index()
+    {
+        // タスク一覧を取得
+        $tasks = Task::all();         // 追加
+
+        // タスク一覧ビューでそれを表示
+        return view('tasks.index', [     // 追加
+            'tasks' => $tasks,        // 追加
+        ]);                                 // 追加
+    }
+
+    // getでtasks/createにアクセスされた場合の「新規登録画面表示処理」
+    public function create()
+    {
+        $task = new task;
+
+        // タスク作成ビューを表示
+        return view('tasks.create', [
+            'task' => $task,
+        ]);
+    }
+
+    // postでtasks/にアクセスされた場合の「新規登録処理」
+    public function store(Request $request)
+    {
+        // バリデーション
+        $request->validate([
+            'status' => 'required|max:10',   // 追加
+    
+        ]);
+        
+        // タスクを作成
+        $task = new Task;
+        $task->status = $request->status; 
+        $task->content = $request->content;
+        $task->save();
+
+        // トップページへリダイレクトさせる
+        return redirect('/');
+    }
+
+    // getでtasks/（任意のid）にアクセスされた場合の「取得表示処理」
+    public function show($id)
+    {
+       // idの値でタスクを検索して取得
+        $task = Task::findOrFail($id);
+
+        // タスク詳細ビューでそれを表示
+        return view('tasks.show', [
+            'task' => $task,
+        ]);
+    }
+
+    // getでtasks/（任意のid）/editにアクセスされた場合の「更新画面表示処理」
+    public function edit($id)
+    {
+       // idの値のタスクを検索して取得
+        $task = Task::findOrFail($id);
+
+        // タスク編集ビューでそれを表示
+        return view('tasks.edit', [
+            'task' => $task,
+        ]);
+    }
+
+    // putまたはpatchでtasks/（任意のid）にアクセスされた場合の「更新処理」
+    public function update(Request $request, $id)
+    {
+        // バリデーション
+        $request->validate([
+            'status' => 'required|max:10',   // 追加
+            
+        ]);
+        
+        
+        // idの値でタスクを検索して取得
+        $task = Task::findOrFail($id);
+        // タスクを更新
+        $task->status = $request->status;    // 追加
+        $task->content = $request->content;
+        $task->save();
+
+        // トップページへリダイレクトさせる
+        return redirect('/');
+    }
+
+    // deleteでtasks/（任意のid）にアクセスされた場合の「削除処理」
+    public function destroy($id)
+    {
+        // idの値でタスクを検索して取得
+        $task = Task::findOrFail($id);
+        // タスクを削除
+        $task->delete();
+
+        // トップページへリダイレクトさせる
+        return redirect('/');
+    }
+}
